f0390f1b6373c2733a06c978e8aefc3bfa342f40
Raushanmehta <raushanmehta2184@gmail.com>
2026-03-23 01:58:59 +0530

    cloudnary error

--- config/cloudinary.js
+++
@@ -1,7 +0,0 @@
-import cloudinary from "cloudinary"
-
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KRY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
